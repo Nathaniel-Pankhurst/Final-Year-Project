8b9fe74212966c0955b744fa8af0542ffaac9af8
Nathaniel-Pankhurst <Nathaniel.Pankhurst@outlook.com>
2017-11-12 19:03:42 +0000

    Created Test abstract class

---
+++ Code/TypeTrack/TypeTrack/TestModels/TestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTrack.TestModels
+{
+    public abstract class TestModel : ITestModel
+    {
+        public KeyValuePair<uint, string> GetCurrentWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<uint, string> GetTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
