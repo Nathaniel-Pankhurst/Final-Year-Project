37a3b52cc1016ee1e4e3ce6a5f1ee6a4a7dd96ec
Nathaniel-Pankhurst <Nathaniel.Pankhurst@outlook.com>
2017-11-12 18:56:08 +0000

    Added User model

---
+++ Code/TypeTrack/TypeTrack/TestModels/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTrack.TestModels
+{
+    class UserModel : IUserModel
+    {
+        public int AddScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAverageScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLatestScore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
