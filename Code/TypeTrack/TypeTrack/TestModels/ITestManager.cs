8c323f9ea83631da63d57c75fa821a6eb0553024
Nathaniel-Pankhurst <Nathaniel.Pankhurst@outlook.com>
2018-02-20 13:29:47 +0000

    Starting on the TestManager interface that should help make sample tests a reality

---
+++ Code/TypeTrack/TypeTrack/TestModels/ITestManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTrack.TestModels
+{
+    interface ITestManager
+    {
+        string GetCurrentWord();
+        string AdvanceWord();
+        string IsLastWord();
+        string GetSampleText();
+        void RepeatTest();
+        void StartNewTest();
+    }
+}
