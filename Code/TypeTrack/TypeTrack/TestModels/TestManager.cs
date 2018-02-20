7d4f10dbe71ce0ede71538b69336fa53188bff1b
Nathaniel Pankhurst <nathaniel.pankhurst@outlook.com>
2018-02-20 14:34:36 +0000

    Creating TestManager based on Interface

---
+++ Code/TypeTrack/TypeTrack/TestModels/TestManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTrack.TestModels
+{
+    class TestManager : ITestManager
+    {
+        public TestManager()
+        {
+        }
+
+        public string AdvanceWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSampleText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string IsLastWord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RepeatTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartNewTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
